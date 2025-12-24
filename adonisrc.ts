b0af86e3c23ce4c2619e098259a9d58da52235d3
lschvn <louis.leschevin@gmail.com>
2025-12-24 12:20:22 +0000

    refactor: reorder AdonisJS kernel and routes preloads

--- adonisrc.ts
+++ adonisrc.ts
@@ -65,7 +65,7 @@ export default defineConfig({
   | List of modules to import before starting the application.
   |
   */
-  preloads: [() => import('#start/routes'), () => import('#start/kernel')],
+  preloads: [() => import('#start/kernel'), () => import('#start/routes')],
 
   /*
   |--------------------------------------------------------------------------
