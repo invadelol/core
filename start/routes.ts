5f536df52413ab0a7c3f174cf1ae81676fb6e69e
lschvn <louis.leschevin@gmail.com>
2026-01-02 18:01:10 +0000

    fix: docs

--- start/routes.ts
+++ start/routes.ts
@@ -50,40 +50,11 @@ router
 router.put('/summoners/puuid/:puuid/increment', [SummonersController, 'incrementViews'])
 
 router.get('/swagger', async () => {
-  // @ts-expect-error
-  return AutoSwagger.docs(router.toJSON(), swagger)
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
 })
 
-router.get('/docs', async ({ response }) => {
-  const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Invade API - Documentation</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
-  <style>
-    body { margin: 0; padding: 0; }
-    .swagger-ui .topbar { display: none; }
-  </style>
-</head>
-<body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js"></script>
-  <script>
-    window.onload = () => {
-      SwaggerUIBundle({
-        url: '/swagger',
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
-        layout: 'BaseLayout'
-      });
-    };
-  </script>
-</body>
-</html>`
-  return response.header('Content-Type', 'text/html').send(html)
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
 })
 
 router.get('/health', [HealthChecksController, 'handle'])
